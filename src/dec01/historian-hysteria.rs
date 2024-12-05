42305551bbcdde716d244f071c71f220f6aec56c
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2024-12-05 01:25:46 -0500

    first commit, beginning advent of code

---
+++ src/dec01/historian-hysteria.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // open file for read
+    // form two lists, left and right
+    // sort left and right lists in ascending order
+    // extract location ids in order, calculate distance
+    // at to total sum
+}
+
