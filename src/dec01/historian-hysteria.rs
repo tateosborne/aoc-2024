ac7c940d6c70442f5a08f8a3d55fcc062be69f65
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2024-12-14 22:15:32 -0500

    open file

--- src/dec01/historian-hysteria.rs
+++ src/dec01/historian-hysteria.rs
@@ -1,5 +1,11 @@
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
     // open file for read
+    let fname = "lists.txt";
+    let path = Path::new(fname);
+    let mut file = File::open(path)
     // form two lists, left and right
     // sort left and right lists in ascending order
     // extract location ids in order, calculate distance
